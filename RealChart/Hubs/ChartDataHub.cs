084db96f08e760be236a7d8bc4da07accbb0edc8
techbrij <techbrij@gmail.com>
2012-10-20 07:05:11 +0530

    Base Version

---
+++ RealChart/Hubs/ChartDataHub.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SignalR.Hubs;
+
+namespace TechBrij.RealChart
+{
+    [HubName("chartData")]
+    public class ChartDataHub : Hub
+    {
+        private readonly ChartData _pointer;
+
+        public ChartDataHub() : this(ChartData.Instance) { }
+
+        public ChartDataHub(ChartData pointer)
+        {
+            _pointer = pointer;
+        }
+
+        public IEnumerable<int> initData()
+        {
+            return _pointer.initData();
+        }       
+    }
+}
